b8bf5dca7ce57b90b05d555c0bba512ecd855400
Veronica Valdez <vvaldez626@gmail.com>
2022-02-17 23:27:55 +0800

    post, get, and put routes and controllers of api

--- api/controllers/song.js
+++ api/controllers/song.js
@@ -1,4 +1,3 @@
-// The "User" variable is defined using a capitalized letter to indicate that what we are using is the "User" model for code readability
 const Song = require("../models/Song");
 
 //Add new song
@@ -10,9 +9,34 @@ module.exports.addSong = (reqBody) => {
   });
   return newSong.save().then((user, error) => {
     if (error) {
-      return false;
+      return error;
     } else {
-      return true;
+      return "New Song Added";
     }
   });
+};
+
+//Get all songs
+module.exports.getAll = () => {
+  return Song.find({}).then((result) => {
+    return result;
+  });
+};
+
+//update song info
+module.exports.updateSong = (reqParams, reqBody) => {
+  let updatedSong = {
+    title: reqBody.title,
+    artist: reqBody.artist,
+    lyrics: reqBody.lyrics,
+  };
+  return Song.findByIdAndUpdate(reqParams.id, updatedSong).then(
+    (song, error) => {
+      if (error) {
+        return error;
+      } else {
+        return "Song was updated";
+      }
+    }
+  );
 };
--- api/routes/song.js
+++ api/routes/song.js
@@ -2,8 +2,22 @@ const express = require("express");
 const router = express.Router();
 const songController = require("../controllers/song");
 
+//add new song
 router.post("/newSong", (req, res) => {
   songController.addSong(req.body).then((result) => res.send(result));
 });
 
+//get all songs
+router.get("/all", (req, res) => {
+  songController.getAll().then((result) => res.send(result));
+});
+
+//update song info
+router.put("/:id", (req, res) => {
+  songController
+    .updateSong(req.params, req.body)
+    .then((result) => res.send(result));
+});
+
+//delete song
 module.exports = router;
